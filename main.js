a00166d08bea32270fb0a1e63c2da7a17be893af
Miguel Urena <urena_miguel82@outlook.com>
2024-05-15 13:10:34 -0700

    Hello file

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const multiply = (a, b) => a * b;
 let n = multiply(5, 8);
 console.log(n);
+
+const fs = require('fs');
+console.log(typeof fs);
+
+fs.writeFile('./hello.text', 'Hello!', function() {
+  console.log('done creating file');
+});
